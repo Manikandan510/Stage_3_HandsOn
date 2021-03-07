14734d5fe77167740a0dff532a3c2c2727083ebd
Manikandan_K <Manikandan510@users.noreply.github.com>
2021-03-07 11:15:26 +0530

    ASP.NET CORE

---
+++ 5_ASP_CORE_HANDSON/CodeAnalysis/CodeAnalysis/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 19;
+        }
+    }
+}
---
+++ 5_ASP_CORE_HANDSON/OrganizationNames/OrganizationNames/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrganizationNames.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
