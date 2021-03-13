dabae28175c2915ba871631ae9b960ab7e23b1a6
Manikandan_K <Manikandan510@users.noreply.github.com>
2021-03-13 12:14:26 +0530

    Handson

---
+++ Nunit CaseStudy/GenericMedicine/GenericMedicineTests/Tests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GenericMedicine;
+namespace GenericMedicineTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Program p;
+        Medicine medicine_carton;
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            p = new Program();
+            medicine_carton = p.CreateMedicineDetail("Dolo 650", "Paracetamol", "Something", DateTime.Parse("2022/01/01"), 100.00);
+        }
+
+        [Test]
+        [TestCase("Dolo 650", "Paracetamol", "Something","2022/01/02",100.00)]
+        public void Medicine_ValidINputs_ObjectCreated(string name, string genericMedicineName, string composition, string expiryDate, double pricePerStrip)
+        {
+            Medicine medicine = p.CreateMedicineDetail(name, genericMedicineName, composition, DateTime.Parse(expiryDate), pricePerStrip);
+            Assert.That(medicine, Is.TypeOf<Medicine>());
+        }
+
+
+        [Test]
+        [TestCase("Dolo 650", "", "Something", "2022 /01/02", 100.00)]
+        public void Medicine_EmptyGenericMedicineName_Exception(string name,string genericMedicineName, string composition, string expiryDate, double pricePerStrip)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, DateTime.Parse(expiryDate), pricePerStrip));
+            Assert.That(ex.Message, Is.EqualTo("Generic Medicine name cannot be empty. Please provide valid value"));
+            
+        }
+
+
+        [Test]
+        [TestCase("Dolo 650", "Paracetamol", "Something", "2022/01/02", 0)]
+        public void Medicine_PriceInValidInputs_Exception(string name, string genericMedicineName, string composition, string expiryDate, double pricePerStrip)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, DateTime.Parse(expiryDate), pricePerStrip));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect value for Medicine price per strip. Please provide valid value"));
+        }
+
+
+        [Test]
+        [TestCase("Dolo 650", "Paracetamol", "Something", "2020/01/02", 100.00)]
+        public void Medicine_DateInValidINputs_Exception(string name, string genericMedicineName, string composition, string expiryDate, double pricePerStrip)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, DateTime.Parse(expiryDate), pricePerStrip));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect expiry date. Please provide valid value"));
+        }
+
+
+
+        [Test]
+        [TestCase(10,"2021/10/01","Chennai")]
+        public void Carton_ValidInput_ObjectCreated(int medicineStripCount, string launchDate, string retailerAddress)
+        {
+            CartonDetail carton = p.CreateCartonDetail(medicineStripCount, DateTime.Parse(launchDate), retailerAddress, medicine_carton);
+            Assert.That(carton, Is.TypeOf<CartonDetail>());
+        }
+
+
+
+        [Test]
+        [TestCase(-5, "2021/10/01", "Bangalore")]
+        [TestCase(null, "2021/10/01", "Hyderabad")]
+        public void Carton_StripCountInvalid_Exception(int medicineStripCount, string launchDate, string retailerAddress)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, DateTime.Parse(launchDate), retailerAddress, medicine_carton));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect strip count. Please check"));
+        }
+
+
+
+        [Test]
+        [TestCase(10, "2020/10/01", "Chennai")]
+        public void Carton_DateInValid_Exception(int medicineStripCount, string launchDate, string retailerAddress)
+        {
+       
+            var ex = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, DateTime.Parse(launchDate), retailerAddress, medicine_carton));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect launch date. Please provide valid value"));
+        }
+
+
+
+        [Test]
+        [TestCase(10, "2022/10/01", "Chennai")]
+        public void Carton_MedicineObjectNull_Test(int medicineStripCount, string launchDate, string retailerAddress)
+        {
+            Medicine medicine_carton = null;
+            CartonDetail carton= p.CreateCartonDetail(medicineStripCount, DateTime.Parse(launchDate), retailerAddress, medicine_carton);
+            Assert.That(carton, Is.EqualTo(null));
+        }
+
+   
+    }
+}
---
+++ Nunit CaseStudy/MyFoodSupply/MyFoodSupplyTests/Tests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFoodSupply;
+using NUnit.Framework;
+namespace MyFoodSupplyTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Program p;
+        FoodDetail fooditem;
+        [SetUp]
+        public void SetUp()
+        {
+            p = new Program();
+            fooditem = p.CreateFoodDetail("Curd Rice", 10, DateTime.Parse("2022/01/02"), 100.00);
+        }
+
+        [Test]
+        [TestCase("Curd Rice", 10, "2022/01/02", 100.00)]
+        public void FoodDetail_ValidInputs_ObjectCreated(string name, int dishType, string expiryDate, double price)
+        {
+            var food = p.CreateFoodDetail(name, dishType, DateTime.Parse(expiryDate), price);
+            Assert.That(food, Is.TypeOf<FoodDetail>());
+        }
+
+
+        [Test]
+        [TestCase("", 10, "2022/01/02", 100.00)]
+        [TestCase(null, 30, "2023/01/02", 80.00)]
+        public void FoodDetail_EmptyName_Exception(string name, int dishType, string expiryDate, double price)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, DateTime.Parse(expiryDate), price));
+            Assert.AreEqual("Dish name cannot be empty. Please provide valid value", ex.Message);
+        }
+
+
+        [Test]
+
+        [TestCase("Curd Rice", 30, "2023/01/02", -120.00)]
+        public void FoodDetail_PriceInvalidInput_Exception(string name, int dishType, string expiryDate, double price)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, DateTime.Parse(expiryDate), price));
+            Assert.AreEqual("Incorrect value for dish price. Please provide valid value", ex.Message);
+        }
+
+
+
+        [Test]
+        [TestCase("Curd Rice", 10, "2020/01/02", 100.00)]
+        public void FoodDetail_DateInvalidInput_Exception(string name, int dishType, string expiryDate, double price)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, DateTime.Parse(expiryDate), price));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", ex.Message);
+        }
+
+
+        [Test]
+        [TestCase(3, "2021/10/01", "FoodQuest", 100.00)]
+        public void SupplyDetail_ValidInput_ObjectCreated(int foodItemCount, String requestDate, string sellerName, double packingCharge)
+        {
+            var supplyDetail = p.CreateSupplyDetail(foodItemCount, DateTime.Parse(requestDate), sellerName, packingCharge, fooditem);
+            Assert.That(supplyDetail, Is.TypeOf<SupplyDetail>());
+        }
+
+
+        [Test]
+        [TestCase(-1, "2021/10/01", "FoodQuest", 100.00)]
+        public void SupplyDetail_CountInvalid_Exception(int foodItemCount, String requestDate, string sellerName, double packingCharge)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, DateTime.Parse(requestDate), sellerName, packingCharge, fooditem));
+            Assert.AreEqual("Incorrect food item count. Please check", ex.Message);
+        }
+
+
+        [Test]
+        [TestCase(5, "2018/10/01", "FoodQuest", 140.00)]
+        public void SupplyDetail_DateInvalid_Exception(int foodItemCount, String requestDate, string sellerName, double packingCharge)
+        {
+            var ex = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, DateTime.Parse(requestDate), sellerName, packingCharge, fooditem));
+            Assert.AreEqual("Incorrect food request date. Please provide valid value", ex.Message);
+        }
+
+
+        [Test]
+        [TestCase(3, "2022/10/01", "FoodQuest", 100.00)]
+        public void SupplyDetail_ObjectNull_Null(int foodItemCount, String requestDate, string sellerName, double packingCharge)
+        {
+            fooditem = null;
+            var supply = p.CreateSupplyDetail(foodItemCount, DateTime.Parse(requestDate), sellerName, packingCharge, fooditem);
+            Assert.AreEqual(null, supply);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            fooditem = null;
+        }
+
+    }
+}
