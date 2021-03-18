44f075a9d5b16a26594d09947b71f3b3038eadde
Manikandan_K <Manikandan510@users.noreply.github.com>
2021-03-18 12:40:45 +0530

    Design Principles

---
+++ 6_Design_Patterns/ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeaveRequest leave = new LeaveRequest()
+            {
+                LeaveDays = 6,
+                Employee = "Mani"
+            };
+            ILeaveRequestHandler leaves = new Supervisor();
+            leaves.HandlerRequest(leave);
+
+            Console.ReadLine();
+        }
+    }
+}
