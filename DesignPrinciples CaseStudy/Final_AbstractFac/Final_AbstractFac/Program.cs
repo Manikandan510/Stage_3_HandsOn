09f8b98aed628016161042cc834a8ed19ec73cb2
Manikandan_K <Manikandan510@users.noreply.github.com>
2021-03-19 13:08:47 +0530

    Final CaseStudy

---
+++ DesignPrinciples CaseStudy/Final_AbstractFac/Final_AbstractFac/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_AbstractFac
+{
+    public enum Product 
+    {
+        Electronic_Products,Toys,Furniture
+    }
+    public enum Channel 
+    {
+        ECommerceWebsite,TeleCallerAgents
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory fact = new ConcreteFactory();
+            Client client = new Client(fact);
+            client.MakeElectronics(Channel.ECommerceWebsite);
+            Console.WriteLine("---------------");
+            client.MakeFuniture(Channel.TeleCallerAgents);
+            Console.WriteLine("---------------");
+            client.MakeToys(Channel.ECommerceWebsite);
+            Console.WriteLine("---------------");
+            Console.ReadKey();
+        }
+    }
+}
